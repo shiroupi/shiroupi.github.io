24b80367ed7997d3d51be43aaa73b9b035743dc0
shiroupi <85266946+shiroupi@users.noreply.github.com>
2021-11-30 12:08:58 -0500

    styling

--- nav.js
+++ nav.js
@@ -1,10 +1,10 @@
-document.write('<div>\
+document.write('<div class="topp">\
     <link rel="stylesheet" href="nav.css">\
-    <h3 class="topp"><ins>Navigation</ins></h3>\
+    <h3><ins>Navigation</ins></h3>\
     <nav>\
-        <p><a class="navv" href="https://shiroupi.github.io">Home</a></p>\
-        <p><a class="navv" href="CompSciA0.html">Testing Grounds</a></p>\
-        <p><a class="navv" href="CompSciB0.html">AP Calculus BC</a></p>\
+        <p><a href="https://shiroupi.github.io">Home</a></p>\
+        <p><a href="CompSciA0.html">Testing Grounds</a></p>\
+        <p><a href="CompSciB0.html">AP Calculus BC</a></p>\
     </nav>\
     </div>\
 ');
